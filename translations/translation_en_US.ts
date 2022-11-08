614145673cf151da9e49385510bf1f5789c93b9d
acalvo99 <60424207+acalvo99@users.noreply.github.com>
2022-11-08 21:07:01 +0100

    Add files via upload

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -1,1 +1,43 @@
-jh
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Desempolva todo tu conocimiento y contesta a estas preguntas de diferentes temas en el menor tiempo posible. Estoy listo para empezar, pulsa comenzar juego.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Desempolva todo tu conocimiento y contesta a estas preguntas de diferentes temas en el menor tiempo posible. Estoy listo para empezar, pulsa comenzar juego.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say Rules</name>
+        <message>
+            <source>Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo. Cuando estés listo para empezar con el juego pulsa continuar.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo. Cuando estés listo para empezar con el juego pulsa continuar.</translation>
+        </message>
+        <message>
+            <source>Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo que tardes en contestarlas. Cuando estés listo para empezar con el juego pulsa continuar.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo que tardes en contestarlas. Cuando estés listo para empezar con el juego pulsa continuar.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Game Introduction</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Desempolva todo tu conocimiento y contesta a estas preguntas de diferentes temas en el menor tiempo posible. Estoy listo para empezar, pulsa comenzar juego.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Desempolva todo tu conocimiento y contesta a estas preguntas de diferentes temas en el menor tiempo posible. Estoy listo para empezar, pulsa comenzar juego.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Game Rules</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo que tardes en contestarlas. Cuando estés listo para empezar con el juego pulsa continuar.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Las reglas del juego son las siguientes: Tendrás solo 30 segundos para cada pregunta. Una vez que selecciones tu respuesta, no se puede deshacer. No puedes seleccionar ninguna opción una vez que pasa el tiempo. No puedes salir de la prueba mientras juegas. Y obtendrás puntos en base a tus respuestas correctas y el tiempo que tardes en contestarlas. Cuando estés listo para empezar con el juego pulsa continuar.</translation>
+        </message>
+    </context>
+</TS>
